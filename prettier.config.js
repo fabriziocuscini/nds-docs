7303e79425d297a772dd0b0e6d1799e3aafe5ae5
Fabrizio Cuscini <fabriziocuscini@me.com>
2023-09-27 13:33:55 +0100

    Update config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: 'lf',
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  printWidth: 100,
+  singleAttributePerLine: false,
+};
